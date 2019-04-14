05e9e863a1bba6d8cd4d0bff589b41447d92e506
3epochs <1738547868@qq.com>
2019-04-14 23:18:17 +0800

    test utils

--- utils.py
+++ utils.py
@@ -71,13 +71,14 @@ def write_results(prediction, confidence, num_classes, nms_conf=0.4):
 
     for ind in range(batch_size):
         image_pred = prediction[ind]
+
         max_conf, max_conf_score = torch.max(image_pred[:, 5:5+num_classes], 1)
         max_conf = max_conf.float().unsqueeze(1)
         max_conf_score = max_conf_score.float().unsqueeze(1)
         seq = (image_pred[:, :5], max_conf, max_conf_score)
         image_pred = torch.cat(seq, 1)
 
-        non_zero_idx = torch.nonzero(image_pred[:, 4])
+        non_zero_idx = (torch.nonzero(image_pred[:, 4]))
         try:
             image_pred_ = image_pred[non_zero_idx.sequeeze(), :].view(-1, 7)
         except:
@@ -87,18 +88,20 @@ def write_results(prediction, confidence, num_classes, nms_conf=0.4):
             continue
 
         img_classes = unique(image_pred_[:, -1])
+
         for cls in img_classes:
             cls_mask = image_pred_ * (image_pred_[:, -1] == cls).float().unsqueeze(1)
             class_mask_idx = torch.nonzero(cls_mask[:, -2]).squeeze()
             image_pred_class = image_pred_[class_mask_idx].view(-1, 7)
 
-            conf_sort_index = torch.sort(image_pred_class[:, 4], descending=True)
+            conf_sort_index = torch.sort(image_pred_class[:, 4], descending=True)[1]
             image_pred_class = image_pred_class[conf_sort_index]
+
             idx = image_pred_class.size(0)  # num of detections
 
             for i in range(idx):
                 try:
-                    ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_[i+1:])
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
                 except ValueError:
                     break
                 except IndexError:
@@ -148,5 +151,33 @@ def bbox_iou(box1, box2):
     b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
 
     iou = inter_area / (b1_area + b2_area - inter_area)
+
     return iou
 
+
+def load_classes(namesfile):
+    fp = open(namesfile, 'r')
+    names = fp.read().split('\n')[:-1]
+    return names
+
+
+def letterbox_image(img, inp_dim):
+    ''' resize image with unchanged aspect ratio using padding'''
+    img_w, img_h = img.shape[1], img.shape[0]
+    w, h = inp_dim
+    new_w = int(img_w * min(w / img_w, h / img_h))
+    new_h = int(img_h * min(w / img_w, h / img_h))
+    resized_image = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_CUBIC)
+
+    canvas = np.full((inp_dim[1], inp_dim[0], 3), 128)
+
+    canvas[(h - new_h) // 2:(h - new_h) // 2 + new_h, (w - new_w) // 2:(w - new_w) // 2 + new_w, :] = resized_image
+
+    return canvas
+
+
+def prep_image(img, inp_dim):
+    img = letterbox_image(img, (inp_dim, inp_dim))
+    img = img[:, :, ::-1].transpose((2, 0, 1)).copy()
+    img = torch.from_numpy(img).float().div(255.0).unsqueeze(0)
+    return img
