64a981af5cfc04c0d66a495065dcf7c7d71387a3
3epochs <1738547868@qq.com>
2019-04-14 11:53:45 +0800

    finish utils.py

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA=True):
 
     prediction = prediction.view(batch_size, bbox_sttrs * num_anchors, grid_size * grid_size)
     prediction = prediction.transpose(1, 2).contiguous()
-    prediction = prediction.view(batch_size, grid_size * grid_size*num_anchors, bbox_sttrs)
+    prediction = prediction.view(batch_size, grid_size * grid_size * num_anchors, bbox_sttrs)
 
     anchors = [(a[0] / stride, a[1] / stride) for a in anchors]
 
@@ -41,7 +41,7 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA=True):
 
     prediction[:, :, :2] += x_y_offset
 
-    anchors = torch.FloatTenor(anchors)
+    anchors = torch.FloatTensor(anchors)
 
     if CUDA:
         anchors = anchors.cuda()
@@ -53,4 +53,100 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA=True):
 
     return prediction
 
+
+def write_results(prediction, confidence, num_classes, nms_conf=0.4):
+    conf_mask = (prediction[:, :, 4] > confidence).float().unsqueeze(2)
+    prediction = prediction * conf_mask  # prediction.shape: Batch_size * 10647 * 85
+
+    box_corner = prediction.new(prediction.shape)
+    box_corner[:, :, 0] = (prediction[:, :, 0] - prediction[:, :, 2] / 2)
+    box_corner[:, :, 1] = (prediction[:, :, 1] - prediction[:, :, 3] / 2)
+    box_corner[:, :, 2] = (prediction[:, :, 0] + prediction[:, :, 2] / 2)
+    box_corner[:, :, 3] = (prediction[:, :, 1] + prediction[:, :, 3] / 2)
+    prediction[:, :, :4] = box_corner[:, :, :4]
+
+    batch_size = prediction.size(0)
+
+    write = False
+
+    for ind in range(batch_size):
+        image_pred = prediction[ind]
+        max_conf, max_conf_score = torch.max(image_pred[:, 5:5+num_classes], 1)
+        max_conf = max_conf.float().unsqueeze(1)
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+        seq = (image_pred[:, :5], max_conf, max_conf_score)
+        image_pred = torch.cat(seq, 1)
+
+        non_zero_idx = torch.nonzero(image_pred[:, 4])
+        try:
+            image_pred_ = image_pred[non_zero_idx.sequeeze(), :].view(-1, 7)
+        except:
+            continue
+
+        if image_pred_.shape[0] == 0:
+            continue
+
+        img_classes = unique(image_pred_[:, -1])
+        for cls in img_classes:
+            cls_mask = image_pred_ * (image_pred_[:, -1] == cls).float().unsqueeze(1)
+            class_mask_idx = torch.nonzero(cls_mask[:, -2]).squeeze()
+            image_pred_class = image_pred_[class_mask_idx].view(-1, 7)
+
+            conf_sort_index = torch.sort(image_pred_class[:, 4], descending=True)
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)  # num of detections
+
+            for i in range(idx):
+                try:
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_[i+1:])
+                except ValueError:
+                    break
+                except IndexError:
+                    break
+
+                iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                image_pred_class[i+1:] *= iou_mask
+
+                non_zero_idx = torch.nonzero(image_pred_class[:, 4]).sequeeze()
+                image_pred_class = image_pred_class[non_zero_idx].view(-1, 7)
+
+        batch_idx = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
+        seq = batch_idx, image_pred_class
+
+        if not write:
+            output = torch.cat(seq, 1)
+            write = True
+        else:
+            out = torch.cat(seq, 1)
+            output = torch.cat((output, out))
+    try:
+        return output
+    except:
+        return 0
+
+
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+    return tensor_res
+
+
+def bbox_iou(box1, box2):
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.max(b1_x2, b2_x2)
+    inter_rect_y2 = torch.max(b1_y2, b2_y2)
+
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+    b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area)
+    return iou
 
