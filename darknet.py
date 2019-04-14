2ffd9501e107c0ee63c12f0907aa7882fe1c31cf
3epochs <1738547868@qq.com>
2019-04-14 11:53:25 +0800

    finish darknet

--- darknet.py
+++ darknet.py
@@ -5,12 +5,138 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
+import cv2
+
+from utils import *
+
+
+def get_test_input():
+    img = cv2.imread("dog-cycle-car.png")
+    img = cv2.resize(img, (416, 416))
+    img_ = img[:, :, ::-1].transpose((2, 0, 1))
+    img_ = img_[np.newaxis, :, :, :] / 255.0
+    img_ = torch.from_numpy(img_).float()
+    img_ = Variable(img_)
+    return img_
+
+
+class Darknet(nn.Module):
+
+    def __init__(self, cfgfile):
+        super(Darknet, self).__init__()
+        self.blocks = parse_cfg(cfgfile)
+        self.net_info, self.module_list = create_modules(self.blocks)
+
+    def forward(self, x, CUDA):
+        modules = self.blocks[1:]
+        outputs = {}
+        write = 0     # indicate whether we have met our first detection
+        for i, module in enumerate(modules):
+            module_type = (module['type'])
+            if module_type == 'convolutional' or module_type == 'upsample':
+                x = self.module_list[i](x)
+
+            elif module_type == 'route':
+                layers = module['layers']
+                layers = [int(a) for a in layers]
+                if layers[0] > 0:
+                    layers[0] = layers[0] - i
+                if len(layers) == 1:
+                    x = outputs[i + (layers[0])]
+                else:
+                    if layers[1] > 0:
+                        layers[1] = layers[1] - i
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+                    x = torch.cat((map1, map2), 1)
+
+            elif module_type == 'shortcut':
+                from_ = int(module['from'])
+                x = outputs[i-1] + outputs[i + from_]
+
+            elif module_type == 'yolo':
+                anchors = self.module_list[i][0].anchors
+                inp_dim = int(self.net_info['height'])
+                num_classes = int(module['classes'])
+
+                x = x.data
+                if CUDA:
+                    x = x.cuda()
+                x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
+                if not write:
+                    detections = x
+                    write = 12
+                else:
+                    detections = torch.cat((detections, x), 1)
+            outputs[i] = x
+
+        return detections
+
+    def load_weights(self, weightfile):
+        fp = open(weightfile, 'rb')
+        header = np.fromfile(fp, dtype=np.int32, count=5)
+        self.header = torch.from_numpy(header)
+        self.seen = self.header[3]
+
+        weights = np.fromfile(fp, dtype=np.float32)
+        ptr = 0
+        for i in range(len(self.module_list)):
+            module_type = self.blocks[i+1]['type']
+            if module_type == 'convolutional':
+                model = self.module_list[i]
+                try:
+                    batch_normalize = int(self.blocks[i+1]['batch_normalize'])
+                except:
+                    batch_normalize = 0
+                conv = model[0]
+
+                if batch_normalize:
+                    bn = model[1]
+                    num_bn_biases = bn.bias.numel()
+                    bn_biases = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_weights = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_mean = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_var = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_biases = bn_biases.view_as(bn.bias.data)
+                    bn_weights = bn_weights.view_as(bn.weight.data)
+                    bn_running_mean = bn_running_mean.view_as(bn.running_mean)
+                    bn_running_var = bn_running_var.view_as(bn.running_var)
+
+                    bn.bias.data.copy_(bn_biases)
+                    bn.weight.data.copy_(bn_weights)
+                    bn.running_mean.copy(bn_running_mean)
+                    bn.running_var.copy_(bn_running_var)
+
+                else:
+                    num_biases = conv.bias.numel()
+                    conv_biases = torch.from_numpy(weights[ptr: ptr + num_biases])
+                    ptr += num_biases
+
+                    conv_biases = conv_biases.view_as(conv.bias.data)
+
+                    conv.bias.data.copy_(conv_biases)
+
+                # load weights for convolutional layers
+                num_weights = conv.weight.numel()
+                conv_weights = torch.from_numpy(weights[ptr: ptr + num_weights])
+                ptr += num_weights
+
+                conv_weights = conv_weights.view_as(conv.weight.data)
+                conv.weight.data.copy_(conv_weights)
 
 
 def parse_cfg(cfgfile):
     """
     function parse cfg file
-    :param cfgfile:
+    :param: cfgfile
     :return: a list of blocks, each block describe a block in the neural network
     to be built.
     """
@@ -59,7 +185,7 @@ def create_modules(blocks):
             stride = int(x['stride'])
 
             if padding:
-                pad = (kernel_size - 1) / 2
+                pad = (kernel_size - 1) // 2
             else:
                 pad = 0
 
@@ -132,6 +258,13 @@ class EmptyLayer(nn.Module):
         super(EmptyLayer, self).__init__()
 
 
-# test
-blocks = parse_cfg('cfg/yolov3.cfg')
-print(create_modules(blocks))
+# # test
+# blocks = parse_cfg('cfg/yolov3.cfg')
+# print(create_modules(blocks))
+
+model = Darknet("cfg/yolov3.cfg")
+model.load_weights('yolov3.weights')
+inp = get_test_input()
+pred = model(inp, torch.cuda.is_available())
+print(pred)
+print(pred.size())
