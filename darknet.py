7def3af1f6a18b267a3a981e5ada68ee71609e21
3epochs <1738547868@qq.com>
2019-04-15 21:21:52 +0800

    add comments to darknet

--- darknet.py
+++ darknet.py
@@ -29,13 +29,15 @@ class Darknet(nn.Module):
 
     def forward(self, x, CUDA):
         modules = self.blocks[1:]
-        outputs = {}
+        outputs = {}  # cache outputs for route layer
         write = 0     # indicate whether we have met our first detection
         for i, module in enumerate(modules):
             module_type = (module['type'])
+            # for conv and upsample layer, just forward through this module
             if module_type == 'convolutional' or module_type == 'upsample':
                 x = self.module_list[i](x)
 
+            # deal with route layer, this is where we actually route
             elif module_type == 'route':
                 layers = module['layers']
                 layers = [int(a) for a in layers]
@@ -50,16 +52,19 @@ class Darknet(nn.Module):
                     map2 = outputs[i + layers[1]]
                     x = torch.cat((map1, map2), 1)
 
+            # deal with shortcut(residual)
             elif module_type == 'shortcut':
                 from_ = int(module['from'])
                 x = outputs[i-1] + outputs[i + from_]
 
+            # deal with yolo layers
             elif module_type == 'yolo':
                 anchors = self.module_list[i][0].anchors
                 inp_dim = int(self.net_info['height'])
                 num_classes = int(module['classes'])
 
                 x = x.data
+                # cuda must be checked, otherwise you'll get a error calling func predict_transform()
                 if CUDA:
                     x = x.cuda()
                 x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
@@ -73,15 +78,27 @@ class Darknet(nn.Module):
         return detections
 
     def load_weights(self, weightfile):
+        """
+        loading weights from pretrained weightsfile, in this case ,we use
+        coco 80 categories weightsfile
+        :param weightfile: pretrained weightsfile
+        :return: None, load weights to our model
+        """
         fp = open(weightfile, 'rb')
+
+        # The first 160 bytes of the weights file store 5 int32 values which
+        # constitute the header of the file
         header = np.fromfile(fp, dtype=np.int32, count=5)
         self.header = torch.from_numpy(header)
         self.seen = self.header[3]
 
+        # store the rest weight file as a ndarray
         weights = np.fromfile(fp, dtype=np.float32)
         ptr = 0
         for i in range(len(self.module_list)):
             module_type = self.blocks[i+1]['type']
+
+            # load weights when we meet conv layers since weights only exist at conv layer
             if module_type == 'convolutional':
                 model = self.module_list[i]
                 try:
@@ -93,7 +110,9 @@ class Darknet(nn.Module):
                 if batch_normalize:
                     bn = model[1]
 
+                    # get num of weights of bn layer
                     num_bn_biases = bn.bias.numel()
+                    # load weights to torch tensor
                     bn_biases = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
                     ptr += num_bn_biases
 
@@ -106,11 +125,13 @@ class Darknet(nn.Module):
                     bn_running_var = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
                     ptr += num_bn_biases
 
+                    # cast the loaded weights so that dims match model
                     bn_biases = bn_biases.view_as(bn.bias.data)
                     bn_weights = bn_weights.view_as(bn.weight.data)
                     bn_running_mean = bn_running_mean.view_as(bn.running_mean)
                     bn_running_var = bn_running_var.view_as(bn.running_var)
 
+                    # copy to model
                     bn.bias.data.copy_(bn_biases)
                     bn.weight.data.copy_(bn_weights)
                     bn.running_mean.copy_(bn_running_mean)
@@ -137,7 +158,7 @@ class Darknet(nn.Module):
 def parse_cfg(cfgfile):
     """
     function parse cfg file
-    :param: cfgfile
+    :param cfgfile: configurations file which describe it's architecture
     :return: a list of blocks, each block describe a block in the neural network
     to be built.
     """
@@ -165,6 +186,10 @@ def parse_cfg(cfgfile):
 
 
 def create_modules(blocks):
+    """
+    :param blocks:  blocks returned by func parser_cfg()
+    :return: module list and network information described by blocks
+    """
     net_info = blocks[0]
     module_list = nn.ModuleList()
     prev_filters = 3
@@ -172,6 +197,8 @@ def create_modules(blocks):
 
     for idx, x in enumerate(blocks[1:]):
         module = nn.Sequential()
+
+        # conv module
         if x['type'] == 'convolutional':
             activation = x['activation']
             try:
@@ -202,11 +229,13 @@ def create_modules(blocks):
                 activ = nn.LeakyReLU(0.1, inplace=True)
                 module.add_module('leaky_{0}'.format(idx), activ)
 
+        # upsample module
         elif x['type'] == 'upsample':
             stride = int(x['stride'])
             upsample = nn.Upsample(scale_factor=stride, mode='nearest')
             module.add_module('upsample_{0}'.format(idx), upsample)
 
+        # route module, cache this position(index)
         elif x['type'] == 'route':
             x['layers'] = x['layers'].split(',')
             start = int(x['layers'][0])
@@ -226,10 +255,12 @@ def create_modules(blocks):
             else:
                 filters = output_filters[idx + start]
 
+        # shortcut module, like ResNet
         elif x['type'] == 'shortcut':
             shortcut = EmptyLayer()
             module.add_module('shortcut_{0}'.format(idx), shortcut)
 
+        # yolo module, detection module
         elif x['type'] == 'yolo':
             mask = x['mask'].split(',')
             mask = [int(x) for x in mask]
